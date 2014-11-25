769cccea1a7c8ba71acc6981ff4731712e471879
Daniel Liu <dliu2@tek.com>
2014-11-25 16:37:11 +0800

    db refactor, phase 1, done. Not fully complete. user/sp/course should be enough, but order not.

--- src/main/scripts/db/create.sql
+++ src/main/scripts/db/create.sql
@@ -1,4 +1,5 @@
 -- Initialize create database tables
+-- total 29 tables 
 
 
 -- ------------------------------------------------------
@@ -6,7 +7,7 @@
 -- ------------------------------------------------------
 
 -- TABLE: USER
-CREATE TABLE user
+CREATE TABLE usr
 (
   id INT UNSIGNED AUTO_INCREMENT,
   loginname VARCHAR(64),
@@ -39,7 +40,7 @@ CREATE TABLE consume_user
   street VARCHAR(128),
   apartment VARCHAR(128),
   description VARCHAR(512),
-  CONSTRAINT consu_user_fk_1 FOREIGN KEY (userid) REFERENCES user(id)
+  CONSTRAINT consu_user_fk_1 FOREIGN KEY (userid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
 
 
@@ -61,23 +62,7 @@ CREATE TABLE coach_user
   photo VARCHAR(128),
   servicearea VARCHAR(255),
   status TINYINT,
-  CONSTRAINT coach_user_fk_1 FOREIGN KEY (userid) REFERENCES user(id)
-) DEFAULT CHARSET = utf8;
-
-
--- TABLE: COACH_CERTIFICATION
-CREATE TABLE coach_certification
-(
-  userid INT UNSIGNED,
-  sportid INT UNSIGNED,
-  content VARCHAR(512),
-  certification VARCHAR(255),
-  createtime TIMESTAMP,
-  verifycontent VARCHAR(512),
-  verifytime TIMESTAMP,
-  status TINYINT,
-  CONSTRAINT cert_user_fk_1 FOREIGN KEY (userid) REFERENCES user(id),
-  CONSTRAINT cert_sport_fk_1 FOREIGN KEY (sportid) REFERENCES sport_type(id)
+  CONSTRAINT coach_user_fk_1 FOREIGN KEY (userid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
 
 
@@ -92,23 +77,24 @@ CREATE TABLE picture
 ) DEFAULT CHARSET = utf8;
 
 
--- TABLE: PICTURE_REL
-CREATE TABLE picture_rel
-(
-  picid BIGINT UNSIGNED,
-  userid INT UNSIGNED,
-  courseid BIGINT UNSIGNED,
-  createtime TIMESTAMP,
-  type TINYINT(1),
-  CONSTRAINT picture_fk_1 FOREIGN KEY (picid) REFERENCES picture(id),
-  CONSTRAINT picture_to_user_fk_2 FOREIGN KEY (userid) REFERENCES user(id),
-  CONSTRAINT picture_to_course_fk_3 FOREIGN KEY (courseid) REFERENCES course(id)
-) DEFAULT CHARSET = utf8;
-
-
 -- ------------------------------------------------------
 --  SP/SPORT relative tables
 -- ------------------------------------------------------
+
+-- TABLE: SP
+CREATE TABLE sp
+(
+  id INT UNSIGNED AUTO_INCREMENT,
+  name VARCHAR(128),
+  description VARCHAR(255),
+  userid BIGINT UNSIGNED,
+  status  TINYINT,
+  createtime TIMESTAMP,
+  verifytime TIMESTAMP,
+  CONSTRAINT sp_pk PRIMARY KEY (id),
+  CONSTRAINT sp_to_user_fk_1 FOREIGN KEY (userid) REFERENCES usr(id)
+) DEFAULT CHARSET = utf8;
+
 
 -- TABLE: SPORT_TYPE
 CREATE TABLE sport_type
@@ -123,26 +109,28 @@ CREATE TABLE sport_type
 
 
 -- TABLE: SPORT_USER_REL
-CREATE TABLE SPORT_USER_REL
+CREATE TABLE sport_user_rel
 (
   sportid INT UNSIGNED,
   userid INT UNSIGNED,
   CONSTRAINT sport_fk_1 FOREIGN KEY (sportid) REFERENCES sport_type(id),
-  CONSTRAINT user_fk_2 FOREIGN KEY (userid) REFERENCES user(id)
+  CONSTRAINT user_fk_2 FOREIGN KEY (userid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
 
--- TABLE: SP
-CREATE TABLE sp
+
+-- TABLE: COACH_CERT_REL
+CREATE TABLE coach_cert_rel
 (
-  id INT UNSIGNED AUTO_INCREMENT,
-  name VARCHAR(128),
-  description VARCHAR(255),
-  userid BIGINT UNSIGNED,
-  status  TINYINT,
+  userid INT UNSIGNED,
+  sportid INT UNSIGNED,
+  content VARCHAR(512),
+  certification VARCHAR(255),
   createtime TIMESTAMP,
+  verifycontent VARCHAR(512),
   verifytime TIMESTAMP,
-  CONSTRAINT sp_pk PRIMARY KEY (id),
-  CONSTRAINT sp_to_user_fk_1 FOREIGN KEY (userid) REFERENCES user(id)
+  status TINYINT,
+  CONSTRAINT cert_user_fk_1 FOREIGN KEY (userid) REFERENCES usr(id),
+  CONSTRAINT cert_sport_fk_1 FOREIGN KEY (sportid) REFERENCES sport_type(id)
 ) DEFAULT CHARSET = utf8;
 
 
@@ -167,7 +155,7 @@ CREATE TABLE course
   id BIGINT UNSIGNED AUTO_INCREMENT,
   spid INT,
   stid INT,
-  teacherid INT,
+  coachid INT,
   coursename VARCHAR(255),
   coursedetail VARCHAR(512),
   courseaddress VARCHAR(255),
@@ -180,31 +168,46 @@ CREATE TABLE course
   actualperson INT,
   courseprice DECIMAL(10,2),
   discount DECIMAL(10,2),
+  status TINYINT,
   coursefans INT,
   courserank INT,
   CONSTRAINT course_pk PRIMARY KEY (id),
-  CONSTRAINT course_to_servprovider_fk_1 FOREIGN KEY (spid) REFERENCES servprovider(id),
-  CONSTRAINT course_to_servtype_fk_2 FOREIGN KEY (stid) REFERENCES servtype(id),
-  CONSTRAINT course_to_coach_fk_3 FOREIGN KEY (teacherid) REFERENCES user(id)
+  CONSTRAINT course_to_sp_fk_1 FOREIGN KEY (spid) REFERENCES sp(id),
+  CONSTRAINT course_to_sport_type_fk_2 FOREIGN KEY (stid) REFERENCES sport_type(id),
+  CONSTRAINT course_to_coach_fk_3 FOREIGN KEY (coachid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
 
 
--- TABLE: COURSE_USER
-CREATE TABLE course_user
+-- TABLE: COURSE_USER_REL
+CREATE TABLE course_user_rel
 (
   courseid BIGINT UNSIGNED,
   studentid INT UNSIGNED,
   createtime TIMESTAMP,
   consumetime TIMESTAMP,
-  evidence VARCHAR(255),
+  receipt VARCHAR(255),
   CONSTRAINT course_user_pk PRIMARY KEY (courseid, studentid),
   CONSTRAINT course_user_fk_1 FOREIGN KEY (courseid) REFERENCES course(id),
-  CONSTRAINT course_user_fk_2 FOREIGN KEY (studentid) REFERENCES user(id)
+  CONSTRAINT course_user_fk_2 FOREIGN KEY (studentid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
 
 
--- TABLE: COMMENT
-CREATE TABLE comment
+-- TABLE: PICTURE_REL
+CREATE TABLE picture_rel
+(
+  picid BIGINT UNSIGNED,
+  userid INT UNSIGNED,
+  courseid BIGINT UNSIGNED,
+  createtime TIMESTAMP,
+  type TINYINT(1),
+  CONSTRAINT picture_fk_1 FOREIGN KEY (picid) REFERENCES picture(id),
+  CONSTRAINT picture_to_user_fk_2 FOREIGN KEY (userid) REFERENCES usr(id),
+  CONSTRAINT picture_to_course_fk_3 FOREIGN KEY (courseid) REFERENCES course(id)
+) DEFAULT CHARSET = utf8;
+
+
+-- TABLE: ASSESSMENT
+CREATE TABLE assessment
 (
   id BIGINT UNSIGNED AUTO_INCREMENT,
   courseid BIGINT UNSIGNED,
@@ -215,7 +218,7 @@ CREATE TABLE comment
   status TINYINT,
   CONSTRAINT comment_pk PRIMARY KEY (id),
   CONSTRAINT comment_to_course_fk_1 FOREIGN KEY (courseid) REFERENCES course(id),
-  CONSTRAINT comment_to_user_fk_2 FOREIGN KEY (userid) REFERENCES user(id)
+  CONSTRAINT comment_to_user_fk_2 FOREIGN KEY (userid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
 
 
@@ -228,8 +231,8 @@ CREATE TABLE attention
   course BIGINT UNSIGNED,  -- 被关注课程
   type TINYINT(1),    -- 1表示关注教练，2表示关注课程
   CONSTRAINT attention_pk PRIMARY KEY (id),
-  CONSTRAINT attention_to_user_fk_1 FOREIGN KEY (fanee) REFERENCES user(id),
-  CONSTRAINT attention_to_user_fk_2 FOREIGN KEY (faner) REFERENCES user(id),
+  CONSTRAINT attention_to_user_fk_1 FOREIGN KEY (fanee) REFERENCES usr(id),
+  CONSTRAINT attention_to_user_fk_2 FOREIGN KEY (faner) REFERENCES usr(id),
   CONSTRAINT attention_to_course_fk_3 FOREIGN KEY (course) REFERENCES course(id)
 ) DEFAULT CHARSET = utf8;
 
@@ -239,7 +242,7 @@ CREATE TABLE attention
 -- ------------------------------------------------------
 
 -- TABLE: NOTICE
-CREATE TABLE NOTICE
+CREATE TABLE notice
 (
   id BIGINT UNSIGNED AUTO_INCREMENT,
   content VARCHAR(1024),
@@ -250,13 +253,13 @@ CREATE TABLE NOTICE
 
 
 -- TABLE: NOTICE_USER
-CREATE TABLE NOTICE_USER
+CREATE TABLE notice_user
 (
   noticeid BIGINT UNSIGNED,
   userid BIGINT UNSIGNED,
   status TINYINT,
   CONSTRAINT notice_fk_1 FOREIGN KEY (noticeid) REFERENCES notice(id),
-  CONSTRAINT notice_to_user_fk_2 FOREIGN KEY (userid) REFERENCES user(id)
+  CONSTRAINT notice_to_user_fk_2 FOREIGN KEY (userid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
 
 
@@ -272,9 +275,8 @@ CREATE TABLE feedback
   replytime TIMESTAMP,
   status TINYINT,
   CONSTRAINT feedback_pk PRIMARY KEY (id),
-  CONSTRAINT feedback_to_user_fk_1 FOREIGN KEY (userid) REFERENCES user(id)
+  CONSTRAINT feedback_to_user_fk_1 FOREIGN KEY (userid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
-
 
 
 -- ------------------------------------------------------
@@ -292,8 +294,8 @@ CREATE TABLE ledgerPayment
   createtime TIMESTAMP,
   updatetime TIMESTAMP,
   CONSTRAINT payment_pk PRIMARY KEY (transtrackingid),
-  CONSTRAINT payment_to_payer_fk_1 FOREIGN KEY (payerid) REFERENCES user(id),
-  CONSTRAINT payment_to_payee_fk_2 FOREIGN KEY (payeeid) REFERENCES user(id)
+  CONSTRAINT payment_to_payer_fk_1 FOREIGN KEY (payerid) REFERENCES usr(id),
+  CONSTRAINT payment_to_payee_fk_2 FOREIGN KEY (payeeid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
 
 
@@ -308,8 +310,8 @@ CREATE TABLE ledgerRefund
   createtime TIMESTAMP,
   updatetime TIMESTAMP,
   CONSTRAINT refund_pk PRIMARY KEY (transtrackingid),
-  CONSTRAINT refund_to_payer_fk_1 FOREIGN KEY (payerid) REFERENCES user(id),
-  CONSTRAINT refund_to_payee_fk_2 FOREIGN KEY (payeeid) REFERENCES user(id)
+  CONSTRAINT refund_to_payer_fk_1 FOREIGN KEY (payerid) REFERENCES usr(id),
+  CONSTRAINT refund_to_payee_fk_2 FOREIGN KEY (payeeid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
 
 
@@ -324,12 +326,12 @@ CREATE TABLE ledgerRelease
   createtime TIMESTAMP,
   updatetime TIMESTAMP,
   CONSTRAINT release_pk PRIMARY KEY (transtrackingid),
-  CONSTRAINT release_to_payer_fk_1 FOREIGN KEY (payerid) REFERENCES user(id),
-  CONSTRAINT release_to_payee_fk_2 FOREIGN KEY (payeeid) REFERENCES user(id)
+  CONSTRAINT release_to_payer_fk_1 FOREIGN KEY (payerid) REFERENCES usr(id),
+  CONSTRAINT release_to_payee_fk_2 FOREIGN KEY (payeeid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
 
 
-CREATE TABLE order
+CREATE TABLE orders
 (
   id BIGINT UNSIGNED AUTO_INCREMENT,
   buyerid INT UNSIGNED,
@@ -340,8 +342,8 @@ CREATE TABLE order
   createtime TIMESTAMP,
   updatetime TIMESTAMP,
   CONSTRAINT order_pk PRIMARY KEY (id),
-  CONSTRAINT order_to_buyer_fk_1 FOREIGN KEY (buyerid) REFERENCES user(id),
-  CONSTRAINT order_to_seller_fk_2 FOREIGN KEY (sellerid) REFERENCES user(id)
+  CONSTRAINT order_to_buyer_fk_1 FOREIGN KEY (buyerid) REFERENCES usr(id),
+  CONSTRAINT order_to_seller_fk_2 FOREIGN KEY (sellerid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
 
 
@@ -353,11 +355,11 @@ CREATE TABLE orderitem
   number INT UNSIGNED,
   amount DECIMAL(10,2),
   refundamount DECIMAL(10,2),
-  CONSTRAINT order_fk_1 FOREIGN KEY (orderid) REFERENCES order(id)
+  CONSTRAINT item_to_order_fk_1 FOREIGN KEY (orderid) REFERENCES orders(id)
 ) DEFAULT CHARSET = utf8;
 
 
-CREATE TABLE orderhist
+CREATE TABLE ordershist
 (
   id BIGINT UNSIGNED AUTO_INCREMENT,
   buyerid INT UNSIGNED,
@@ -368,8 +370,8 @@ CREATE TABLE orderhist
   createtime TIMESTAMP,
   updatetime TIMESTAMP,
   CONSTRAINT orderhist_pk PRIMARY KEY (id),
-  CONSTRAINT orderhist_to_buyer_fk_1 FOREIGN KEY (buyerid) REFERENCES user(id),
-  CONSTRAINT orderhist_to_seller_fk_2 FOREIGN KEY (sellerid) REFERENCES user(id)
+  CONSTRAINT orderhist_to_buyer_fk_1 FOREIGN KEY (buyerid) REFERENCES usr(id),
+  CONSTRAINT orderhist_to_seller_fk_2 FOREIGN KEY (sellerid) REFERENCES usr(id)
 ) DEFAULT CHARSET = utf8;
 
 
@@ -381,8 +383,72 @@ CREATE TABLE orderitemhist
   number INT UNSIGNED,
   amount DECIMAL(10,2),
   refundamount DECIMAL(10,2),
-  CONSTRAINT order_fk_1 FOREIGN KEY (orderid) REFERENCES order(id)
+  CONSTRAINT itemhist_to_order_fk_1 FOREIGN KEY (orderid) REFERENCES orders(id)
 ) DEFAULT CHARSET = utf8;
 
+
+
+-- ------------------------------------------------------
+--  Server side authority management tables
+-- ------------------------------------------------------
+
+CREATE TABLE sys_user
+(
+  id INT UNSIGNED AUTO_INCREMENT,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  email VARCHAR(64),
+  mobile VARCHAR(24),
+  status TINYINT(1),
+  createtime TIMESTAMP,
+  logintime TIMESTAMP,
+  lastlogintime TIMESTAMP,
+  lastlogouttime TIMESTAMP,
+  CONSTRAINT sys_user_pk PRIMARY KEY (id)
+) DEFAULT CHARSET = utf8;
+
+CREATE TABLE role
+(
+  id INT UNSIGNED AUTO_INCREMENT,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  type TINYINT(1),
+  visible TINYINT(1),
+  editable TINYINT(1),
+  CONSTRAINT role_pk PRIMARY KEY (id)
+) DEFAULT CHARSET = utf8;
+
+CREATE TABLE user_role_rel
+(
+  userid INT UNSIGNED,
+  roleid INT UNSIGNED,
+  CONSTRAINT sys_user_fk_1 FOREIGN KEY (userid) REFERENCES sys_user(id),
+  CONSTRAINT role_fk_2 FOREIGN KEY (roleid) REFERENCES role(id)
+) DEFAULT CHARSET = utf8;
+
+
+-- ------------------------------------------------------
+--  System audit tables
+-- ------------------------------------------------------
+
+CREATE TABLE sys_audit_log
+(
+  userid INT UNSIGNED,
+  username VARCHAR(255),
+  subsystem VARCHAR(255),
+  activity VARCHAR(255),
+  keyword VARCHAR(255),
+  description VARCHAR(2018)
+) DEFAULT CHARSET = utf8;
+
+CREATE TABLE usr_activity_log
+(
+  userid INT UNSIGNED,
+  username VARCHAR(255),
+  subsystem VARCHAR(255),
+  activity VARCHAR(255),
+  keyword VARCHAR(255),
+  description VARCHAR(2018)
+) DEFAULT CHARSET = utf8;
 
 COMMIT;
--- src/main/scripts/db/removal.sql
+++ src/main/scripts/db/removal.sql
@@ -1,18 +1,44 @@
--- delete all dps database tables
+-- DROP all dps database tables
 
+DROP TABLE IF EXISTS picture_rel;
+DROP TABLE IF EXISTS SPORT_USER_REL;
+DROP TABLE IF EXISTS course_user_rel;
+DROP TABLE IF EXISTS sp_sport_rel;
+DROP TABLE IF EXISTS coach_cert_rel;
 
-DROP TABLE IF EXISTS servprovider_to_servtype;
-DROP TABLE IF EXISTS user_to_servprovider;
-DROP TABLE IF EXISTS comment;
-DROP TABLE IF EXISTS attention;
-DROP TABLE IF EXISTS message;
-DROP TABLE IF EXISTS bill;
-DROP TABLE IF EXISTS course_user;
-DROP TABLE IF EXISTS coach_user;
-DROP TABLE IF EXISTS consume_user;
+DROP TABLE IF EXISTS sp;
 DROP TABLE IF EXISTS course;
-DROP TABLE IF EXISTS servtype;
-DROP TABLE IF EXISTS servprovider;
-DROP TABLE IF EXISTS user;
+
+DROP TABLE IF EXISTS assessment;
+DROP TABLE IF EXISTS attention;
+DROP TABLE IF EXISTS notice_user;
+
+DROP TABLE IF EXISTS notice;
+DROP TABLE IF EXISTS feedback;
+
+DROP TABLE IF EXISTS sport_type;
+DROP TABLE IF EXISTS picture;
+
+DROP TABLE IF EXISTS consume_user;
+DROP TABLE IF EXISTS coach_user;
+DROP TABLE IF EXISTS usr;
+
+
+DROP TABLE IF EXISTS ledgerPayment;
+DROP TABLE IF EXISTS ledgerRefund;
+DROP TABLE IF EXISTS ledgerRelease;
+
+DROP TABLE IF EXISTS orderitemhist;
+DROP TABLE IF EXISTS ordershist;
+DROP TABLE IF EXISTS orderitem;
+DROP TABLE IF EXISTS orders;
+
+DROP TABLE IF EXISTS sys_audit_log;
+DROP TABLE IF EXISTS usr_activity_log;
+
+DROP TABLE IF EXISTS user_role_rel;
+DROP TABLE IF EXISTS sys_user;
+DROP TABLE IF EXISTS role;
+
 
 COMMIT;
